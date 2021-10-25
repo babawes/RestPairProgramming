988201554c352c6b6687a4d9e6ba1be9a41b827f
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2021-10-25 09:45:21 +0200

    Første omgang

---
+++ RestPairProgramming/Managers/MusicRecordsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestPairProgramming.Models;
+
+namespace RestPairProgramming.Managers
+{
+    public static class MusicRecordsManager
+    {
+        private static List<MusicRecord> Data = new List<MusicRecord>()
+        {
+            new MusicRecord("album1","Artist1",34,25,"Rock"),
+            new MusicRecord("album2","Artist5",346,22,"Pop"),
+            new MusicRecord("album6","Artist2",43,52,"Rock")
+        };
+
+        public static List<MusicRecord> GetAll()
+        {
+            return Data;
+        }
+
+        
+
+    }
+}
---
+++ RestPairProgramming/Models/MusicRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestPairProgramming.Models
+{
+    public class MusicRecord
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+        public string Genre { get; set; }
+
+        public MusicRecord()
+        {
+            
+        }
+
+        public MusicRecord(string title, string artist, int duration, int publicationYear, string genre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            PublicationYear = publicationYear;
+            Genre = genre;
+        }
+    }
+}
